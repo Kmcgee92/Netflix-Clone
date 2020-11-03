7a710bec256882109cce38b2b82e02097c3296c8
kmcgee92 <kaseycmcgee@gmail.com>
2020-11-02 17:47:01 -0700

    alter format test

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
-import Slider from './components/Slider/Slider'
-
-
+import React from "react";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+import Slider from "./components/Slider/Slider";
 
 function App() {
-
-    return (
-        <BrowserRouter>
-        <Slider/>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Slider />
+      <Slider />
+      <Slider />
+      <Slider />
+      <Slider />
+      <Slider />
+      <Slider />
+      <Slider />
+      <Slider />
+    </BrowserRouter>
+  );
 }
 
 export default App;
 
-            // <nav>
-            //     <ul>
-            //         <li><NavLink to="/" activeclass="active">Home</NavLink></li>
-            //         <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
-            //     </ul>
-            // </nav>
-            // <Switch>
-            //     <Route path="/users">
-            //         <UserList />
-            //     </Route>
+// <nav>
+//     <ul>
+//         <li><NavLink to="/" activeclass="active">Home</NavLink></li>
+//         <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
+//     </ul>
+// </nav>
+// <Switch>
+//     <Route path="/users">
+//         <UserList />
+//     </Route>
 
-            //     <Route path="/">
-            //         <h1>My Home Page</h1>
-            //     </Route>
-            // </Switch>
+//     <Route path="/">
+//         <h1>My Home Page</h1>
+//     </Route>
+// </Switch>
